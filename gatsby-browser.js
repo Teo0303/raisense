67cc1c87499973a8e6a00f05180465c75f00bc6e
Jonibekov Azizbek <jonibekov97@gmail.com>
2019-11-21 16:13:03 +0500

    Raisense v1.0

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'swiper/css/swiper.css';
+import './src/style/global.css';
+
 // You can delete this file if you're not using it
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,89 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from 'react';
+import Slider from '../components/Slider/slider';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import ServicesList from '../components/ServicesList/serviceslist';
+import Partners from '../components/PartnersList/partners';
+import Header from '../components/Header/header';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+export const query = graphql`
+	{
+		allPrismicServ {
+			nodes {
+				data {
+					description {
+						html
+						text
+					}
+					image {
+						alt
+						copyright
+						url
+					}
+					name {
+						html
+						text
+					}
+				}
+			}
+		}
+		allPrismicSlider {
+			nodes {
+				data {
+					description {
+						text
+					}
+					image {
+						alt
+						url
+						localFile {
+							childImageSharp {
+								fluid {
+									sizes
+									src
+									srcSet
+								}
+							}
+						}
+					}
+					title {
+						text
+					}
+				}
+			}
+		}
+		allPrismicPartners {
+			nodes {
+				data {
+					image {
+						url
+					}
+					link {
+						url
+						target
+					}
+				}
+			}
+		}
+	}
+`;
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default class IndexPage extends Component {
+	state = {
+		services: this.props.data.allPrismicServ.nodes,
+		partners: this.props.data.allPrismicPartners.nodes,
+		sliders: this.props.data.allPrismicSlider.nodes
+	};
 
-export default IndexPage
+	render() {
+		return (
+			<Layout>
+				<SEO title="Home" />
+				<Slider data={this.state.sliders} />
+				<Header />
+				<ServicesList data={this.state.services} />
+				<Partners data={this.state.partners} />
+			</Layout>
+		);
+	}
+}
